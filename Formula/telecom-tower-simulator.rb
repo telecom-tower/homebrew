1a2be376651b0c99122e1a4247a29f85b0b10042
goreleaserbot <goreleaser@telecomtower.xyz>
2018-03-08 13:37:03 +0100

    telecom-tower-simulator version v1.0.4

--- Formula/telecom-tower-simulator.rb
+++ Formula/telecom-tower-simulator.rb
@@ -1,9 +1,9 @@
 class TelecomTowerSimulator < Formula
   desc ""
   homepage "https://github.com/telecom-tower"
-  url "https://github.com/telecom-tower/simulator/releases/download/v1.0.3/telecom-tower-simulator_1.0.3_macOS_64-bit.tar.gz"
-  version "1.0.3"
-  sha256 "7c542b3a41da3978a5cf4dfa34610ea95ee1a8b92f573e4d05e1da2a038d7c72"
+  url "https://github.com/telecom-tower/simulator/releases/download/v1.0.4/telecom-tower-simulator_1.0.4_macOS_64-bit.tar.gz"
+  version "1.0.4"
+  sha256 "9fde10814fbc735e35a6f4c1f367510a9f6cae22083a43a19be510af4cf41a38"
 
   def install
     bin.install "telecom-tower-simulator"
