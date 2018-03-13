eaee1555c96c53b63d4d59ed02889e2aa2d427f2
goreleaserbot <goreleaser@telecomtower.xyz>
2018-03-13 17:15:34 +0100

    Brew formula update for telecom-tower-simulator version v1.0.8

--- Formula/telecom-tower-simulator.rb
+++ Formula/telecom-tower-simulator.rb
@@ -1,9 +1,9 @@
 class TelecomTowerSimulator < Formula
   desc ""
   homepage "https://github.com/telecom-tower"
-  url "https://github.com/telecom-tower/simulator/releases/download/v1.0.7/telecom-tower-simulator_1.0.7_macOS_64-bit.tar.gz"
-  version "1.0.7"
-  sha256 "c6f41c1de8c17cc0a25eaea010948d8f8f7d323d566c7c4c2f966e02c0bed947"
+  url "https://github.com/telecom-tower/simulator/releases/download/v1.0.8/telecom-tower-simulator_1.0.8_macOS_64-bit.tar.gz"
+  version "1.0.8"
+  sha256 "bbb2cc461cf6352154aa75e6c4fe8ae452d91d4bc341dfdb65895bde15da7c86"
 
   def install
     bin.install "telecom-tower-simulator"
